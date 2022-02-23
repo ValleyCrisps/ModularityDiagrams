d4d2f038f023fbac3dd81c6603506f9706f2d509
ValleyCrisps <36808057+ValleyCrisps@users.noreply.github.com>
2022-02-23 18:23:34 +0900

    first working example

---
+++ README.md
@@ -0,0 +1,20 @@
+# Modularity Diagrams
+
+Reference: 
+- Mathologer video on YouTube https://www.youtube.com/watch?v=6ZrO90AI0c8
+
+## TODO
+
+From Mathologer original wishlist
+- [ ] Being able to color line segments according to length.
+- [ ] Indication of the "direction" of multiplication. 1x2 = 2 and so there should really be a little arrow from 1 to 2 not just a simple connection :)
+- [ ] different loops in different colors.
+
+From me
+- [ ] static page hosting
+- [ ] user input for the modulus
+- [ ] option to see the animation(s)
+- [ ] single loop or all loops option
+- [ ] dark mode
+- [ ] typescript
+- [ ] slider for factor
--- sketch.js
+++ sketch.js
@@ -1,65 +1,80 @@
 const width = 800
 const height = 800
 const radius = 300
-const n = 7
+const modulus = 700
 const factor = 11
 
-let points, path
-
-const computePoints = (n, radius) => {
-  const points ={}
-  for(let i= 0; i< n; i++){
-    const x = radius * Math.cos(2* Math.PI *i/n)
-    const y = radius * Math.sin(2* Math.PI*i/n)
-    points[i] ={x,y}
-  }
-  return points
-}
-
-const computePathArray = (n, factor) =>{
-  // throw if factor is 1 or 0
-  let path = [1]
-  let next = factor % n 
-  while (!path.includes(next)) {
-    path.push(next)
-    next = (next * factor ) % n
-  }
-  path.push(next)
-  return path
-}
-
+let points, path, loops
 
 function setup() {
   createCanvas(width, height)
-  // setCenter(width/2, height/2)
-  points = computePoints(n, radius)
-  path = computePathArray(n, factor)
+  points = computePointCoordinates(modulus, radius)
+  loops = computeLoops(modulus, factor)
+  // console.log(loops)
 }
 
 function draw() {
   // move origin to center
-  translate(width/2, height/2)
+  translate(width / 2, height / 2)
   // flip y-axis so it points upwards
   // scale(1, -1);
-  rotate(-PI/2)
-  circle(0, 0, 2*radius)
-  
+  rotate(-PI / 2)
+  circle(0, 0, 2 * radius)
+
   // draw points on circle
-  for(let i= 0; i< n; i++){
+  for (let i = 0; i < modulus; i++) {
     // point(x, y)
     circle(points[i].x, points[i].y, 5)
-    text(`${i}`,  points[i].x, points[i].y)
+    // text(`${i}`, points[i].x, points[i].y)
   }
   // draw lines
-  for (let i = 0; i<path.length -1; i ++) {
-    const start = points[path[i]]
-    const end = points[path[i+1]]
-    line(start.x, start.y, end.x, end.y)
-  }
+  loops.forEach((path) => {
+    for (let i = 0; i < path.length - 1; i++) {
+      const start = points[path[i]]
+      const end = points[path[i + 1]]
+      arrow(start, end)
+    }
+  })
 }
 
+function computePointCoordinates(modulus, radius) {
+  const points = {}
+  for (let i = 0; i < modulus; i++) {
+    const x = radius * Math.cos((2 * Math.PI * i) / modulus)
+    const y = radius * Math.sin((2 * Math.PI * i) / modulus)
+    points[i] = { x, y }
+  }
+  return points
+}
 
+function computeLoops(modulus, factor) {
+  let loops = [[0]]
+  let visited = [0]
+  let start = 1
+  function computeSingleLoop(modulus, start, factor) {
+    // throw if factor is 1 or 0
+    let loop = [start]
+    let next = (start * factor) % modulus
+    while (!loop.includes(next)) {
+      loop.push(next)
+      next = (next * factor) % modulus
+    }
+    visited = visited.concat(loop)
+    loop.push(next)
+    return loop
+  }
+  while (visited.length < modulus) {
+    if (!visited.includes(start)) {
+      const loop = computeSingleLoop(modulus, start, factor)
+      loops.push(loop)
+    }
+    start++
+  }
+  return loops
+}
 
-
-
-
+function arrow(start, end) {
+  // start, end  of type {x, y}
+  line(start.x, start.y, end.x, end.y)
+  // ???
+}
